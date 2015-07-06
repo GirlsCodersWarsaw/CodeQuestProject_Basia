d5a43a1620746cac7b5ac5df0f07869c3060ebdb
Basia Klosowska <barbaraklosowska@gmail.com>
2015-07-06 16:26:27 +0200

    js to project form - doesn't work #1

---
+++ app/assets/javascripts/projects.js
@@ -0,0 +1,10 @@
+var checkbox = document.getElementById('project_billable');
+  var details_div = document.getElementById('financial_details');
+  checkbox.onchange = function() {
+     if(this.checked) {
+       details_div.style['display'] = 'block';
+     } else {
+       details_div.style['display'] = 'none';
+     }
+  };
+
