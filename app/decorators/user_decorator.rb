087582e114763e209180f247c6133788bc2c71b0
Basia Klosowska <barbaraklosowska@gmail.com>
2015-07-13 12:23:35 +0200

    after merging

--- app/decorators/user_decorator.rb
+++ app/decorators/user_decorator.rb
@@ -1,16 +1,9 @@
 class UserDecorator < Draper::Decorator
   delegate_all
-<<<<<<< HEAD
-  
-def full_name
-	first_name = object.first_name = "first_name" ? "empty first name" : object.first_name
-	last_name = object.last_name = "last_name" ? "empty last_name" : object.last_name
-=======
 
   def full_name
     first_name = object.first_name == "first_name" ? "empty first name" : object.first_name
     last_name = object.last_name == "last_name" ? "empty last_name" : object.last_name
->>>>>>> develop
 
     "#{first_name} #{last_name}"
   end
