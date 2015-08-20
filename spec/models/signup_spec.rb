ba92d3e9956643dba2e60890f4930e8b2cf19d1e
Basia Klosowska <barbaraklosowska@gmail.com>
2015-08-20 16:39:45 +0200

    test validations in signup spec -wip #32

--- spec/models/signup_spec.rb
+++ spec/models/signup_spec.rb
@@ -1,6 +1,7 @@
 require 'rails_helper'
 
 describe Signup do
+
   context 'is valid' do
     before do
       params = { user: {first_name: "Joe",
@@ -23,6 +24,55 @@ describe Signup do
   end
 
   context 'is invalid' do
+    context 'arguments are blank' do
+      before do
+        params = { user: {first_name: "",
+                          last_name: "",
+                          email: "",
+                          password: "",
+                          password_confirmation: "",
+                          company: { company_name: ""}}}
+        @signup = Signup.new(params)
+      end
+
+      it "can't be saved" do
+        expect(@signup).to respond_to(:save)
+        expect(@signup.save).to be_falsy
+      end
+
+      it 'has right errors' do
+        @signup.valid?
+        expect(@signup.errors.full_messages).to include("First name can't be blank",
+                                                        "Last name can't be blank",
+                                                        "Company name can't be blank",
+                                                        "Email can't be blank",
+                                                        "Password can't be blank",
+                                                        "Password is too short (minimum is 8 characters)")
+      end
+    end
+
+    context 'password is too short' do
+      before do
+        @user = create(:user, email: "example@example.com")
+        params = { user: {first_name: "Joe",
+                          last_name: "Doe",
+                          email: "example@example.com",
+                          password: "pass",
+                          password_confirmation: "pass",
+                          company: { company_name: "my company"}}}
+        @signup = Signup.new(params)
+      end
+
+      it "can't be saved" do
+        expect(@signup).to respond_to(:save)
+        expect(@signup.save).to be_falsy
+      end
+
+      it 'has right error' do
+        @signup.valid?
+        expect(@signup.errors.full_messages).to include("Password is too short (minimum is 8 characters)")
+      end
+    end
     context 'email has already been taken' do
       before do
         @user = create(:user, email: "example@example.com")
@@ -33,18 +83,16 @@ describe Signup do
                           password_confirmation: "password",
                           company: { company_name: "my company"}}}
         @signup = Signup.new(params)
-        @signup.instance_eval{ email_is_unique }
       end
 
       it "can't be saved" do
         expect(@signup).to respond_to(:save)
         expect(@signup.save).to be_falsy
-
       end
 
       it 'has right error' do
-        expect(@signup.errors).to be_truthy
-        expect(@signup.errors.full_messages).to eq(["Email has already been taken"])
+        @signup.valid?
+        expect(@signup.errors.full_messages).to include("Email has already been taken")
       end
     end
   end
